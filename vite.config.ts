aebfea12bc7f00e80cf898f4685b06798dbd2a0c
Erlend Oftedal <erlend@oftedal.no>
2023-11-10 14:01:08 +0100

    Try to generate images

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { imagetools } from 'vite-imagetools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "/soccerscore",
+  plugins: [
+    vue(),
+    imagetools({
+      defaultDirectives() {
+        return new URLSearchParams({
+          format: 'webp',
+          w: '180',
+          h: '180'
+        });
+      }
+    })
+  ],
+  base: '/soccerscore',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
