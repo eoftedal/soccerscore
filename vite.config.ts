c3c17f3d8a667c24564969ab7db0879225861e99
Erlend Oftedal <erlend@oftedal.no>
2023-11-10 14:29:56 +0100

    Static images

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,31 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { imagetools } from 'vite-imagetools';
+import { loadImage, applyTransforms, builtins, generateTransforms } from 'vite-imagetools';
+const image = loadImage('./src/assets/soccer.webp');
+
+const versions = [16, 120, 152, 180];
+const favIconPlugin = () => {
+  return {
+    name: 'favIconPlugin',
+    async transformIndexHtml(html: string) {
+      const links = [];
+      for (const v of versions) {
+        const resized = image.resize({ width: v, height: v });
+        const base64 = (await resized.toBuffer()).toString('base64');
+        links.push(
+          `<link rel="icon" type="image/webp" sizes="${v}x${v}" href="data:image/webp;base64,${base64}">`
+        );
+      }
+
+      return html.replace('<link rel="icon">', links.join('\n'));
+    }
+  };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    imagetools({
-      defaultDirectives() {
-        return new URLSearchParams({
-          format: 'webp',
-          w: '180',
-          h: '180'
-        });
-      }
-    })
-  ],
+  plugins: [vue(), favIconPlugin()],
   base: '/soccerscore',
   resolve: {
     alias: {
